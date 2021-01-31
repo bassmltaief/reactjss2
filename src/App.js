e3ec2ae2327790ff2611a0c0a86072a81e8de75a
bassmltaief <bassem.ltaief92@gmail.com>
2021-01-31 20:45:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import { Player } from 'video-react'
+import React from 'react'
 import './App.css';
+import img from './reactsrc.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class = "title_red"> Bassem Ltaief </h1>
+      <a href="#">
+      <h1>image from src folder</h1>
+      <img src={img} alt ='myImage' />
+      </a>
+      
+      <br></br>
+      <a href="#" >
+      <h1>image from public folder</h1>
+      </a>
+      
+      <img src="/reactpub.jpeg" alt="myimage" />
+      <br></br>
+      <a href="#">
+      <h1>watch video</h1>
+      </a>
+      <Player
+      playsInline
+      poster="/assets/poster.png"
+      src="https://www.youtube.com/watch?v=7sDY4m8KNLc"
+    />
+      
+      <br></br>
     </div>
   );
 }
